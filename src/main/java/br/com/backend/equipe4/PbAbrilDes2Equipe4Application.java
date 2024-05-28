6fb35cdccd2df9a0342dcf4c9eb58a54d0921938
Lucas Bernadino <luucasdinoo@gmail.com>
2024-05-28 14:45:53 -0300

    First Commit

---
+++ src/main/java/br/com/backend/equipe4/PbAbrilDes2Equipe4Application.java
@@ -0,0 +1,13 @@
+package br.com.backend.equipe4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PbAbrilDes2Equipe4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PbAbrilDes2Equipe4Application.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/backend/equipe4/PbAbrilDes2Equipe4ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.backend.equipe4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PbAbrilDes2Equipe4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
